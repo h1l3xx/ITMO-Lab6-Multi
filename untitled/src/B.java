d871605fe8431ad91b9f8c01ecf5a0eb0d8cea3f
h1l3xx <alksmlkv@gmail.com>
2022-12-11 23:58:36 +0300

    Без исключений.

---
+++ untitled/src/B.java
@@ -0,0 +1,7 @@
+public class B extends A{
+    int b;
+    public B(int b){
+        this.b = b;
+        System.out.println('2');
+    }
+}
---
+++ untitled1/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public int method(){
+        int x=5;
+    }
+    public void main(String[] args) {
+        int x = method();
+
+
+    }
+
+}
